6f1d14cb445272e14740bfef23cb2aa11100f5d2
quinncidences <qrc.inpa@gmail.com>
2019-05-22 09:52:01 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import './App.css'
-import Form from './Form'
-import Order from './Order'
+import Form from './components/Form'
+import Order from './components/Order'
 
 class App extends Component {
-  state: {
+  constructor() {
+    super()
+    this.state = {
     orders: []
+    }
   }
 
   addOrder = (order) => {
@@ -25,7 +28,7 @@ class App extends Component {
           <img src={ require('./images/logo.png') } className="App-logo" alt="logo" />
         </header>
 
-        <Form />
+        <Form addOrder={ this.addOrder }/>
 
         <div className="ui raised container segment">
           <h1 className="ui block header">All Orders</h1>
